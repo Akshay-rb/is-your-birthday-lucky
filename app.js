6b1973443697e174d7a8052824424a255287dc81
Akshay-rb <akshayrbr2@gmail.com>
2021-08-19 23:28:56 +0530

    added style

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const dateOfBirth = document.querySelector("#date-of-birth")
 const luckyNumber = document.querySelector("#lucky-number")
 const checkNumber = document.querySelector("#check-number")
+const outputText = document.querySelector("#output-text")
 
 const checkBirthdateIsLucky = () =>{
-    const sum = calculateSum(dateOfBirth.value)
-    console.log(sum)
+    const dob = dateOfBirth.value
+    const sum = calculateSum(dob)
+    if(sum&&dob)
+    compareValues(sum, luckyNumber.value)
+    outputText.innerText = "Please enter the data "
+}
+
+const compareValues = (sum, luckyNumber) =>{
+    // console.log(sum,luckyNumber)
+    if(sum%luckyNumber === 0){
+       outputText.innerText = "your lucky"
+    }else{
+        outputText.innerText = "Make your own luck!!"
+    }
 }
 
 const calculateSum = (dob) =>{
